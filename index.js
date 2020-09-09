f852468dbb2101f8ceb27ac4f0967cc6d7913201
Grandon Smith <grandon.smith@yahoo.com>
2020-09-09 10:15:29 -0400

    completed app but dogs repeatedtly stack on each other if page is not refreshed and requests are repeatedly made.

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 
-
-
-
-function getImages(num) {
-    fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
-    .then(response => response.json())
-    .then(responseJson =>
-        console.log(responseJson));
-        
-}
-
-
-
-function waitForSubmit() {
-    $('form').submit(event => {
+function randomDogs() {
+    $('.js-random-dogs').submit(event => {
         event.preventDefault();
         const num = $('#dog-amount').val();
         getImages(num);
@@ -21,8 +8,55 @@ function waitForSubmit() {
 };
 
 
+function getImages(num) {
+    fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
+    .then(response => response.json())
+    .then(responseJson => renderPictures(responseJson)
+      
+    );
+};
+
+function renderPictures(responseJson) {
+    console.log(responseJson);
+
+    for (i = 0; i < responseJson.message.length; i++) {
+        console.log(i);
+ 
+        $('.group').append(`
+        <img src="${responseJson.message[i]}" class="item">
+        `);
+    }
+}
+
+
+
+
+function dogBreeds() {
+    $('.js-dog-breeds').submit(event => {
+        event.preventDefault();
+        const name = $('#dog-breed').val();
+        getBreedImages(name);
+    })
+};
+
+function getBreedImages(name) {
+    fetch(`https://dog.ceo/api/breed/${name}/images/random`)
+    .then(response => response.json())
+    .then(responseJson => renderOnePicture(responseJson)
+    );
+}
+
+function renderOnePicture(responseJson) {
+    console.log(responseJson);
+
+    $('.group').append(`
+        <img src="${responseJson.message}" class="item">
+    `);
+}
+
 
 
 $(function callBack() {
-    waitForSubmit();
+    randomDogs();
+    dogBreeds();
 });
