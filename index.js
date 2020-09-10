53efe36faef35697b304291c0db41ebb544fb111
Grandon Smith <grandon.smith@yahoo.com>
2020-09-10 11:55:08 -0400

    rendered pictures will now replace previous ones, .catch added if pics cannot be retrieved, only inputs 1-50 accepted.

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 
-function randomDogs() {
-    $('.js-random-dogs').submit(event => {
-        event.preventDefault();
-        const num = $('#dog-amount').val();
-        getImages(num);
-    })
-};
 
+function checkValue() {
+    $('.js-random-dogs').submit('#dog-amount', event => {
+        event.preventDefault();
+        const amount = $('#dog-amount').val();
+        console.log(amount);
+        if (amount > 50 || amount < 1) {
+            alert("Please select between 1 and 50 pictures.");
+        } else if (amount < 51) {
+            getImages(amount);
+            clearPrevious()
+        }
+    });
+}
+
+
+function clearPrevious() {
+    $('.group').replaceWith('<div class="group"><div>');
+}
 
 function getImages(num) {
     fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
     .then(response => response.json())
-    .then(responseJson => renderPictures(responseJson)
-      
-    );
+    .then(responseJson => renderPictures(responseJson))
+    .catch(error => alert("Sorry, someone let the dogs out! Couldn't retrieve pictures at this time."));
 };
 
 function renderPictures(responseJson) {
     console.log(responseJson);
-
+    
     for (i = 0; i < responseJson.message.length; i++) {
         console.log(i);
- 
         $('.group').append(`
         <img src="${responseJson.message[i]}" class="item">
         `);
@@ -34,6 +43,7 @@ function renderPictures(responseJson) {
 function dogBreeds() {
     $('.js-dog-breeds').submit(event => {
         event.preventDefault();
+        clearPrevious();
         const name = $('#dog-breed').val();
         getBreedImages(name);
     })
@@ -42,8 +52,8 @@ function dogBreeds() {
 function getBreedImages(name) {
     fetch(`https://dog.ceo/api/breed/${name}/images/random`)
     .then(response => response.json())
-    .then(responseJson => renderOnePicture(responseJson)
-    );
+    .then(responseJson => renderOnePicture(responseJson))
+    .catch(error => alert("Sorry, someone let the dogs out! Couldn't retrieve pictures at this time."));
 }
 
 function renderOnePicture(responseJson) {
@@ -57,6 +67,6 @@ function renderOnePicture(responseJson) {
 
 
 $(function callBack() {
-    randomDogs();
+    checkValue();
     dogBreeds();
 });
